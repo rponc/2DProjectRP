f55538a7c23afe4765a660a1fe8f76159aae184d
rponc <166068876+rponc@users.noreply.github.com>
2024-04-18 14:52:10 -0700

    Class

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    float horizontalDir;
+
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.velocity = new Vector2(x: horizontalDir * speed, rb.velocity.y);
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 inputDir = value.Get<Vector2>();
+        float inputX = inputDir.x;
+
+        horizontalDir = inputX;
+    }
+}
